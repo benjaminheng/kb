0e1fb446d9ca9c6ce6c0d3f01fcf86e71ce386ae
Benjamin Heng <benjamin.heng@thecarousell.com>
2022-01-06 18:34:16 +0800

    Implement `kb new`

--- cmd_new.go
+++ cmd_new.go
@@ -4,9 +4,13 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"os"
+	"path"
 	"regexp"
+	"strings"
 
+	"github.com/benjaminheng/kb/config"
 	"github.com/spf13/cobra"
 )
 
@@ -41,6 +45,20 @@ func createNewFile(cmd *cobra.Command, args []string) error {
 			filename = s
 		}
 	}
+
+	filePath := path.Join(config.Config.General.KnowledgeBaseDir, filename)
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		f, err := os.Create(filePath)
+		if err != nil {
+			return err
+		}
+		io.WriteString(f, fmt.Sprintf("---\ntitle: \"%s\"\n---", title))
+		f.Close()
+		editFileWithWorkingDir(config.Config.General.Editor, filePath, config.Config.General.KnowledgeBaseDir)
+	} else {
+		editFileWithWorkingDir(config.Config.General.Editor, filePath, config.Config.General.KnowledgeBaseDir)
+	}
 	return nil
 }
 
@@ -52,4 +70,16 @@ func titleToFilename(title string) string {
 	filename = strings.ToLower(filename)
 	filename += ".md"
 	return filename
+}
+
+// validate that file exists and is not a directory
+func validateFileExists(filePath string) error {
+	fileInfo, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		return fmt.Errorf("%s does not exist", filePath)
+	}
+	if fileInfo.IsDir() {
+		return fmt.Errorf("%s is a directory", filePath)
+	}
+	return nil
 }
